37c4d2e0beefdaa416864507d44ea6b8b1e48e8b
AndikaVikarMuhamad <susinurlaelasari2@gmail.com>
2022-06-29 20:10:05 +0700

    My-portfolio

---
+++ dist/js/script.js
@@ -0,0 +1,47 @@
+// tombol
+const navMenu = document.querySelector("#nav-menu");
+const tombol = document.querySelector("#tombol");
+tombol.addEventListener("click", function () {
+  tombol.classList.toggle("tombol-aktif");
+  navMenu.classList.toggle("hidden");
+});
+// end of tombol
+// navbar
+window.onscroll = function () {
+  const toTop = document.querySelector("#to-Top");
+  const header = document.querySelector("header");
+  const fixednav = header.offsetTop;
+  if (window.pageYOffset > fixednav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
+  console.log("navbar fixed");
+};
+// Dark
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#7dd3fc",
+        violet7: "#8b5cf6", //400
+        violet6: "#c4b5fd", //300
+        violet5: "#ede9fe", //100
+        violet4: "#ddd6fe", //200
+        violet2: "#6b21a8", //p800
+        violet3: "#7c3aed", //600
+        violet: "#7c3aed",
+        kuning: "#FFCB00",
+        biru: "#14b8a6",
+        sky: "#7dd3fc",
+        ungu: "#8b5cf6",
+        danger: "#facc15",
+        Yellow: "#eab308",
+        dark: "#1e293b",
+        hitam: "#0f172a",
+        abu: "#64748b",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
